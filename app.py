5e56696a25a5b81bb89ba9ef5e1d24fb3afaa6c6
Vasilii Filin <55667050+FVA13@users.noreply.github.com>
2023-08-26 15:31:31 +0300

    data preprocessing updated

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ import numpy as np
 
 from models.TransformerModel import get_best_matches
 
-# from PIL import Image
-
-# icon = Image.open("icon.png")
-
 st.set_page_config(
     page_title="Find the best Matching Addresses",
     # page_icon=icon,
@@ -21,40 +17,10 @@ st.set_page_config(
 )
 
 
-# # @st.cache_data
-# def predict(file, model_chosen):
-#     # df = pd.read_csv()
-#     # X, y = df[:-2], df[-1:]
-#     X, y = make_classification(n_features=20, n_samples=1000, shuffle=True, random_state=21)
-#     train, test = get_train_test_for_torch(X, y)
-#     # if model_chosen == "Neural Network":
-#     model = LitClassifier.load_from_checkpoint(
-#         "notebooks/lightning_logs/version_7/checkpoints/epoch=1-step=1400.ckpt",
-#         feautures_num=X.shape[1],
-#     )
-#     trainer = L.Trainer(max_epochs=2)
-#     trainer.fit(model, data.DataLoader(train))
-#     prediction = trainer.predict(
-#         model,
-#         data.DataLoader(torch.from_numpy(X).to(torch.float32)) #, num_workers=12)
-#     )
-#     return prediction
-
-
-# def main():
-#     file_uploaded = st.file_uploader("File uploader", type=["csv"])
-#     model_chosen = st.selectbox("Select Option", ["Neural Network", "CatBoost"])
-#     calculate = st.button('Calculate')
-#     if calculate is True:
-#         st.spinner()
-#         with st.spinner(text='In progress'):
-#             prediction = predict(file_uploaded, model_chosen)
-#             st.table(response_to_numpy(prediction))
-#         st.success('Done')
-
 def main():
     query = st.text_input("Enter some text")
     calculate = st.button("Fetch Matches")
+    # add file upload
     with open("notebooks/embeddings_list.pkl", "rb") as f:
         embeddings_list = pickle.load(f)
 
