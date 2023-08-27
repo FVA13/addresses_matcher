699d0b71ec7b3c0cf7f594e89d9a1b25559ba7af
Vasilii Filin <55667050+FVA13@users.noreply.github.com>
2023-08-27 08:11:53 +0300

    upd work on API and Advanced Model

---
+++ app_flask.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from models.TransformerModel import get_best_matches_json
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods = ['POST'])
+def result():
+    request_json = request.json
+    prediction = get_best_matches_json()
+    return jsonify({'prediction': str(prediction)})
+
+if __name__ == '__main__':
+    app.run()
+
+
+# {
+#     "success": true,
+# “query:”:[
+#     "address": "аптерский 18 спб"
+# ],
+# "result": [
+#     {
+# “target_building_id”: 209676 ,
+# “target_address”:  “г.Санкт-Петербург, Аптекарский проспект, дом 18, литера А” }
+# ]
+# }
