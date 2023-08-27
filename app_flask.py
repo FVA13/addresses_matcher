c6476abc1c4d2da455c3ef4da77756bb841c45dd
Vasilii Filin <55667050+FVA13@users.noreply.github.com>
2023-08-27 09:41:57 +0300

    upd

--- app_flask.py
+++ app_flask.py
@@ -4,13 +4,14 @@ from models.TransformerModel import get_best_matches_json
 app = Flask(__name__)
 
 
-@app.route('/predict', methods = ['POST'])
+@app.route("/predict", methods=["POST"])
 def result():
     request_json = request.json
-    prediction = get_best_matches_json()
-    return jsonify({'prediction': str(prediction)})
+    prediction = get_best_matches_json(request_json)
+    return jsonify({"prediction": str(prediction)})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
 
 
--- models/AdvancedPrecisionLayer.py
+++ models/AdvancedPrecisionLayer.py
@@ -3,11 +3,22 @@ import pandas as pd
 from pullenti_wrapper.langs import set_langs, RU
 from pullenti_wrapper.processor import Processor, GEO, ADDRESS
 from pullenti_wrapper.referent import Referent
+from catboost import CatBoostClassifier, Pool, metrics
+from sklearn.model_selection import train_test_split
+
+from TransformerModel import *
 
 set_langs([RU])
 
 
 processor = Processor([GEO, ADDRESS])
+
+
+def coalesce(s: pd.Series, *series):
+    """coalesce the column information like a SQL coalesce."""
+    for other in series:
+        s = s.mask(pd.isnull, other)
+    return s
 
 
 # recursive function
@@ -62,3 +73,126 @@ def get_address_ner_objects(address: str):
         return merged_data
     except:
         return []
+
+
+def input_transformation(user_input):
+    user_input = pd.DataFrame(
+        columns=[
+            "город",
+            "улица",
+            "дом",
+            "поселок",
+            "проспект",
+            "корпус",
+            "Литера",
+            "муниципальный район",
+            "набережная",
+            "шоссе",
+            "парк",
+            "переулок",
+            "площадь",
+            "аллея",
+            "",
+            "линия",
+            "автодорога",
+            "микрорайон",
+            "деревня",
+            "проезд",
+            "квартал",
+            "бульвар",
+            "станция",
+            "район",
+            "территория",
+            "муниципальный округ",
+            "мост",
+            "тупик",
+            "область",
+            "село",
+            "поселок городского типа",
+            "округ",
+            "волость",
+        ]
+    )  # rewrite
+    user_res = pd.concat(
+        [user_input, pd.DataFrame(get_address_ner_objects(QUERY), index=[0])]
+    )
+    user_res["ner_city"] = coalesce(
+        user_res["город"],
+        user_res["поселок"],
+        user_res["деревня"],
+        user_res["село"],
+        user_res["поселок городского типа"],
+    )
+    user_res["ner_street"] = coalesce(
+        user_res["улица"],
+        user_res["проспект"],
+        user_res["набережная"],
+        user_res["шоссе"],
+        user_res["парк"],
+        user_res["переулок"],
+        user_res["площадь"],
+        user_res["аллея"],
+        user_res["линия"],
+        user_res["автодорога"],
+        user_res["проезд"],
+        user_res["бульвар"],
+    )
+    user_res["ner_house"] = user_res["дом"]
+    user_res["ner_corpus"] = user_res["корпус"]
+    user_res["ner_liter"] = user_res["Литера"]
+    user_res["ner_district"] = coalesce(
+        user_res["муниципальный район"],
+        user_res["микрорайон"],
+        user_res["квартал"],
+        user_res["район"],
+        user_res["муниципальный округ"],
+    )
+    user_res["ner_area"] = coalesce(
+        user_res["станция"], user_res["территория"], user_res["мост"], user_res["тупик"]
+    )
+    user_res["ner_subject"] = coalesce(
+        user_res["область"], user_res["округ"], user_res["волость"]
+    )
+    user_res = user_res[[col for col in user_res if col.startswith("ner")]]
+    return user_res
+
+
+def get_advanced_matching_scores(input_query, embeddings: np.array, sentences):
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    raw_ranking = get_best_matches(input_query, 3, embeddings, sentences)
+    raw_ranking_ids = raw_ranking.id_building
+    query = input_transformation(input_query).dropna(axis=1)
+    mapping = pd.read_pickle("../data/processed/mapping_id_building_index.pkl")
+    raw_ranking_indices = mapping.query("id_building in @raw_ranking_ids").index
+    for col in query.columns:
+        col_value = query[col].values[0]
+        query_col_embedding = model.encode(
+            col_value, convert_to_tensor=True, normalize_embeddings=True
+        )
+        with open("../data/processed/embeddings_{}.pkl".format(col), "rb") as f:
+            db_col_embedding = np.array(pickle.load(f)[list(raw_ranking_indices)])
+        raw_ranking.loc[:, "score_" + col] = db_col_embedding.dot(
+            query_col_embedding.T
+        ).ravel()
+    raw_ranking.loc[:, "original_query"] = input_query
+    return raw_ranking
+
+
+def multiple_advanced_matching_scores(df, embeddings: np.array, sentences):
+    res = pd.DataFrame()
+    for index, row in df.iterrows():
+        i_address = row.address.replace("(", "").replace(")", "")
+        i_df = get_advanced_matching_scores(i_address, embeddings, sentences)
+        i_df.loc[:, "y_correct"] = i_df.id_building == row.target_building_id
+        res = pd.concat([res, i_df])
+    return res.reset_index(drop=True)
+
+
+def get_advanced_pred(data, embeddings, for_embeddings):
+    model = CatBoostClassifier()
+    model.load_model('../models/CatBoostClassifier')
+    data = multiple_advanced_matching_scores(
+        data, np.array(embeddings), for_embeddings
+    ).drop(columns=["address", "id_building"])
+    result = model.predict(data)
+    return result
